d02303ed27622dbbf63ea6137ebd0aa9c2a6c69e
TJ Holowaychuk <tj@vision-media.ca>
2012-03-21 11:20:02 -0700

    typo

--- test/dimensions.js
+++ test/dimensions.js
@@ -2,7 +2,7 @@
 var Parser = require('../')
   , dim = Parser.dimensions;
 
-describe('ms', function(){
+describe('dimensions', function(){
   it('should return an object with width/height', function(){
     dim('', '200x400').should.eql({ width: 200, height: 400 });
   })
