b66b65ce6f814239d3665e5763814bcfadf8b2f1
TJ Holowaychuk <tj@vision-media.ca>
2012-03-21 14:01:14 -0700

    Added glob plugin

---
+++ test/glob.js
@@ -0,0 +1,23 @@
+
+var Parser = require('../')
+  , glob = Parser.glob;
+
+describe('glob', function(){
+  it('should perform a sync glob', function(){
+    glob('', 'glob: test/fixtures/*.js').should.eql(
+      ['test/fixtures/bar.js',
+       'test/fixtures/baz.js',
+       'test/fixtures/foo.js']
+    );
+
+    glob('', 'glob: test/fixtures/b*.js').should.eql(
+      ['test/fixtures/bar.js',
+       'test/fixtures/baz.js']
+    );
+
+    glob('', 'glob: test/fixtures/{bar,foo}.js').should.eql(
+      ['test/fixtures/bar.js',
+       'test/fixtures/foo.js']
+    );
+  })
+})
