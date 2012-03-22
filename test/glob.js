ca2259c6a830ce8efaaf3f574d662c2fe3bdef4b
TJ Holowaychuk <tj@vision-media.ca>
2012-03-21 19:36:09 -0700

    Removed semi-colons from glob and include

--- test/glob.js
+++ test/glob.js
@@ -4,18 +4,18 @@ var Parser = require('../')
 
 describe('glob', function(){
   it('should perform a sync glob', function(){
-    glob('', 'glob: test/fixtures/*.js').should.eql(
+    glob('', 'glob test/fixtures/*.js').should.eql(
       ['test/fixtures/bar.js',
        'test/fixtures/baz.js',
        'test/fixtures/foo.js']
     );
 
-    glob('', 'glob: test/fixtures/b*.js').should.eql(
+    glob('', 'glob test/fixtures/b*.js').should.eql(
       ['test/fixtures/bar.js',
        'test/fixtures/baz.js']
     );
 
-    glob('', 'glob: test/fixtures/{bar,foo}.js').should.eql(
+    glob('', 'glob test/fixtures/{bar,foo}.js').should.eql(
       ['test/fixtures/bar.js',
        'test/fixtures/foo.js']
     );
