00df5ac1a26a477d5ab9f9004391fcd30b66fb78
Tim Oxley <secoif@gmail.com>
2012-07-20 16:43:43 +1000

    Add globbing files feature to include plugin

--- test/include.js
+++ test/include.js
@@ -16,4 +16,14 @@ describe('include', function(){
         ["admin", "guest"]
       ]);
   })
+  it('should parse files matching glob', function() {
+    Parser()
+      .use(include)
+      .read('test/fixtures/include_glob.json')
+      .should.eql({
+        "app-config": { "db": "redis", "listen": 8000 },
+        "user-config": { "permissions": { "view videos": "guest", "delete videos": "admin" }, "roles": ["admin", "guest"] },
+        "users":  [ {"username": "Dave"}, {"username": "Tobi"} ]
+      })
+  })
 })
